7538553e2a29bf728b3a9a33e45c1b82e11bca37
spacegroot <106295842+spacegroot@users.noreply.github.com>
2022-05-28 07:37:59 +0530

    Updated matrix.py

--- matrix.py
+++ matrix.py
@@ -1,4 +1,4 @@
 def matrix(ecg,carvasc,gluc):
     ohe = [[0,0,0],[0,0,1],[0,1,0],[1,0,0],[0,1,1],[1,0,1],[1,1,0],[1,1,1]]
     arr = [ecg,carvasc,gluc]
-    return ohe.index([arr])
+    return (ohe.index([arr])+1)*12.5
